4e0efa891a4dd8115e5418b16171c197578fc068
Your Name <>
2019-02-20 15:08:03 +0300

    Init project

---
+++ pw05.task01.Reflection/src/main/java/com/example/reflection/Serialization.java
@@ -0,0 +1,14 @@
+package com.example.reflection;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class Serialization {
+    public static void serialize(Object object, OutputStream out) {
+
+    }
+
+    public static <T> T deserialize(InputStream in, Class<T> clazz) {
+        return null;
+    }
+}
---
+++ pw05.task01.Reflection/src/test/java/com/example/reflection/SerializationTest.java
@@ -0,0 +1,16 @@
+package com.example.reflection;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SerializationTest {
+
+    @Test
+    void testSerialize() {
+    }
+
+    @Test
+    void testDeserialize() {
+    }
+}
