ef45ca731be71f3862a6164d0a21918a61d138c2
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-27 17:20:26 +0300

    add tests

--- pw06/task01.Calculator/src/test/java/com/example/hse/CalculatorTest.java
+++ pw06/task01.Calculator/src/test/java/com/example/hse/CalculatorTest.java
@@ -1,20 +1,97 @@
 package com.example.hse;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InOrder;
 
 import java.util.LinkedList;
 import java.util.List;
 
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
-    private List list;
-    private List spy;
+    @Test
+    void testSpyPlus() {
+        List<Integer> list = new LinkedList<>();
+        List<Integer> spy = spy(list);
 
-    @BeforeEach
-    void setUp() {
-        list = new LinkedList();
-        spy = spy(list);
+        InOrder inOrder = inOrder(spy);
+        String expression = "7 6 +";
+
+        Calculator calculator = new Calculator(spy);
+        assertEquals(13, calculator.calculate(expression));
+
+        inOrder.verify(spy).add(7);
+        inOrder.verify(spy).add(6);
+        inOrder.verify(spy).get(1);
+        inOrder.verify(spy).remove(1);
+        inOrder.verify(spy).get(0);
+        inOrder.verify(spy).remove(0);
+        inOrder.verify(spy).add(13);
+        inOrder.verify(spy).get(0);
+    }
+
+    @Test
+    void testSpyMinus() {
+        List<Integer> list = new LinkedList<>();
+        List<Integer> spy = spy(list);
+
+        InOrder inOrder = inOrder(spy);
+        String expression = "7 13 -";
+
+        Calculator calculator = new Calculator(spy);
+        assertEquals(-6, calculator.calculate(expression));
+
+        inOrder.verify(spy).add(7);
+        inOrder.verify(spy).add(13);
+        inOrder.verify(spy).get(1);
+        inOrder.verify(spy).remove(1);
+        inOrder.verify(spy).get(0);
+        inOrder.verify(spy).remove(0);
+        inOrder.verify(spy).add(-6);
+        inOrder.verify(spy).get(0);
+    }
+
+    @Test
+    void testSpyMultiply() {
+        List<Integer> list = new LinkedList<>();
+        List<Integer> spy = spy(list);
+
+        InOrder inOrder = inOrder(spy);
+        String expression = "7 6 *";
+
+        Calculator calculator = new Calculator(spy);
+        assertEquals(42, calculator.calculate(expression));
+
+        inOrder.verify(spy).add(7);
+        inOrder.verify(spy).add(6);
+        inOrder.verify(spy).get(1);
+        inOrder.verify(spy).remove(1);
+        inOrder.verify(spy).get(0);
+        inOrder.verify(spy).remove(0);
+        inOrder.verify(spy).add(42);
+        inOrder.verify(spy).get(0);
+    }
+
+    @Test
+    void testSpyDiv() {
+        List<Integer> list = new LinkedList<>();
+        List<Integer> spy = spy(list);
+
+        InOrder inOrder = inOrder(spy);
+        String expression = "42 4 /";
+
+        Calculator calculator = new Calculator(spy);
+        assertEquals(10, calculator.calculate(expression));
+
+        inOrder.verify(spy).add(42);
+        inOrder.verify(spy).add(4);
+        inOrder.verify(spy).get(1);
+        inOrder.verify(spy).remove(1);
+        inOrder.verify(spy).get(0);
+        inOrder.verify(spy).remove(0);
+        inOrder.verify(spy).add(10);
+        inOrder.verify(spy).get(0);
     }
 }
