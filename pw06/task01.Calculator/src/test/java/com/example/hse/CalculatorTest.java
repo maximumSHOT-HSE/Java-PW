32bb137de2719a6b8695b55051007150bc007889
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-02-27 17:03:45 +0300

    fix build gradle

--- pw06/task01.Calculator/src/test/java/com/example/hse/CalculatorTest.java
+++ pw06/task01.Calculator/src/test/java/com/example/hse/CalculatorTest.java
@@ -1,6 +1,20 @@
 package com.example.hse;
 
+import org.junit.jupiter.api.BeforeEach;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.mockito.Mockito.spy;
+
 class CalculatorTest {
 
+    private List list;
+    private List spy;
 
+    @BeforeEach
+    void setUp() {
+        list = new LinkedList();
+        spy = spy(list);
+    }
 }
