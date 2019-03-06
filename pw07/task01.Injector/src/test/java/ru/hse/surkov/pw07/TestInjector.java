895dca169f35e117ec9049f1fdfaf339987c4e8b
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-03-06 16:48:59 +0300

    Add cycle check test

--- pw07/task01.Injector/src/test/java/ru/hse/surkov/pw07/TestInjector.java
+++ pw07/task01.Injector/src/test/java/ru/hse/surkov/pw07/TestInjector.java
@@ -2,10 +2,12 @@ package ru.hse.surkov.pw07;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 public class TestInjector {
 
@@ -38,5 +40,14 @@ public class TestInjector {
         assertTrue(object instanceof ClassWithOneInterfaceDependency);
         ClassWithOneInterfaceDependency instance = (ClassWithOneInterfaceDependency) object;
         assertTrue(instance.dependency instanceof InterfaceImpl);
+    }
+
+    @Test
+    void testCycleDependencies() {
+        List<String> implementations = new ArrayList<>();
+        implementations.add("ru.hse.surkov.pw07.A");
+        implementations.add("ru.hse.surkov.pw07.B");
+        implementations.add("ru.hse.surkov.pw07.C");
+        assertThrows(InjectionCycleException.class, () -> Injector.initialize("ru.hse.surkov.pw07.A", implementations));
     }
 }
