2d868018efcc14756c886d5bffb21afe83b74a6d
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-03-13 15:31:40 +0300

    create files

---
+++ pw08/task01.test/src/main/java/ru/hse/surkov/pw08/MyLinkedHashMap.java
@@ -0,0 +1,27 @@
+package ru.hse.surkov.pw08;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.AbstractMap;
+import java.util.Map;
+import java.util.Set;
+
+public class MyLinkedHashMap <K, V> extends AbstractMap implements Map {
+
+    @Nullable
+    @Override
+    public Object put(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public void putAll(@NotNull Map m) {
+
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        return null;
+    }
+}
---
+++ pw08/task01.test/src/test/java/ru/hse/surkov/pw08/MyLinkedHashMapTest.java
@@ -0,0 +1,7 @@
+package ru.hse.surkov.pw08;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyLinkedHashMapTest {
+
+}
