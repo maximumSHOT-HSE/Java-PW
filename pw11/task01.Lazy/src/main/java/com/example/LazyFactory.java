d6a09d397ae191019c9cafa437b2983ae2ab29e6
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-04-10 16:24:42 +0300

    add solution

--- pw11/task01.Lazy/src/main/java/com/example/LazyFactory.java
+++ pw11/task01.Lazy/src/main/java/com/example/LazyFactory.java
@@ -1,6 +1,7 @@
 package com.example;
 
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Supplier;
 
 public class LazyFactory {
@@ -15,12 +16,13 @@ public class LazyFactory {
                 if (data == null) {
                     data = Optional.ofNullable(supplier.get());
                 }
-                return data.get();
+                return data.orElse(null);
             }
         };
     }
 
     public static <T> Lazy<T> createMultiThreadLazy(Supplier<T> supplier) {
+
         return new Lazy<T>() {
 
             private volatile Optional<T> data;
@@ -29,10 +31,34 @@ public class LazyFactory {
             public T get() {
                 if (data == null) {
                     synchronized (this) {
-
+                        if (data == null) {
+                            data = Optional.ofNullable(supplier.get());
+                        }
                     }
                 }
+                return data.orElse(null);
             }
-        }
+        };
+    }
+
+    public static <T> Lazy<T> createLocklessMultiThreadLazy(Supplier<T> supplier) {
+
+        return new Lazy<>() {
+            private AtomicReference<Optional<T>> dataReference = new AtomicReference<>();
+            @Override
+            public T get() {
+                Optional<T> data = dataReference.get();
+
+                if (data == null) {
+                    data = Optional.ofNullable(supplier.get());
+                    if (dataReference.compareAndSet(null, data)) {
+                        return data.orElse(null);
+                    } else {
+                        return dataReference.get().orElse(null);
+                    }
+                }
+                return data.orElse(null);
+            }
+        };
     }
 }
--- pw11/task01.Lazy/src/test/java/com/example/LazyFactoryTest.java
+++ pw11/task01.Lazy/src/test/java/com/example/LazyFactoryTest.java
@@ -1,7 +1,229 @@
 package com.example;
 
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+import java.util.function.Supplier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LazyFactoryTest {
 
+    @Test
+    void createOneThreadLazyOneResultRandom() {
+        Random random = new Random(179);
+        Lazy<Integer> lazy = LazyFactory.createOneThreadLazy(random::nextInt);
+        Integer first = lazy.get();
+        for (int i = 0; i < 179; ++i) {
+            assertEquals(first, lazy.get());
+        }
+    }
+
+    @Test
+    void createOneThreadLazyReturnsNull() {
+        Lazy<String> lazy = LazyFactory.createOneThreadLazy(() -> null);
+        assertNull(lazy.get());
+    }
+
+    @Test
+    void createOneThreadLazyOneCall() {
+        Lazy<String> lazy = LazyFactory.createOneThreadLazy(new ExplodingSupplier());
+        assertDoesNotThrow(lazy::get);
+        for (int i = 0; i < 179; ++i) {
+            assertDoesNotThrow(lazy::get);
+        }
+    }
+
+    @Test
+    void createOneThreadLazyOneCallNull() {
+        Lazy<String> lazy = LazyFactory.createOneThreadLazy(new ExplodingNullSupplier());
+        assertDoesNotThrow(lazy::get);
+        for (int i = 0; i < 179; ++i) {
+            assertDoesNotThrow(lazy::get);
+        }
+    }
+
+    @Test
+    void createOneThreadLazyAdder() {
+        Lazy<Integer> lazy = LazyFactory.createOneThreadLazy(new AddSupplier());
+        Integer first = lazy.get();
+        for (int i = 0; i < 179; ++i) {
+            assertEquals(first, lazy.get());
+        }
+    }
+
+    @RepeatedTest(10)
+    void createMultiThreadLazyRandom() throws InterruptedException {
+        Random random = new Random(179);
+        Lazy<Integer> lazy = LazyFactory.createMultiThreadLazy(random::nextInt);
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        Integer[] result = new Integer[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> result[ii] = lazy.get());
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+    }
+
+    @RepeatedTest(10)
+    void createMultiThreadLazyReturnsNull() {
+        Lazy<String> lazy = LazyFactory.createMultiThreadLazy(() -> null);
+        assertNull(lazy.get());
+    }
+
+    @RepeatedTest(10)
+    void createLocklessMultiThreadLazyOneResultRandom() throws InterruptedException {
+        Random random = new Random(179);
+        Lazy<Integer> lazy = LazyFactory.createLocklessMultiThreadLazy(random::nextInt);
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        Integer[] result = new Integer[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> result[ii] = lazy.get());
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+    }
+
+    @RepeatedTest(10)
+    void createLocklessMultiThreadedLazyAdder() throws InterruptedException {
+        Lazy<Integer> lazy = LazyFactory.createLocklessMultiThreadLazy(new AddSupplier());
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        Integer[] result = new Integer[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> result[ii] = lazy.get());
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+    }
+
+
+
+    @RepeatedTest(10)
+    void createMultiThreadAdder() throws InterruptedException {
+        Lazy<Integer> lazy = LazyFactory.createMultiThreadLazy(new AddSupplier());
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        Integer[] result = new Integer[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> result[ii] = lazy.get());
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+    }
+
+    @RepeatedTest(10)
+    void createLocklessMultiThreadLazyReturnsNull() {
+        Lazy<String> lazy = LazyFactory.createLocklessMultiThreadLazy(() -> null);
+        assertNull(lazy.get());
+    }
+
+    @RepeatedTest(10)
+    void createMultiThreadExploding() throws InterruptedException {
+        Lazy<String> lazy = LazyFactory.createMultiThreadLazy(new ExplodingSupplier());
+        final IllegalStateException[] exception = {null};
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        String[] result = new String[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    result[ii] = lazy.get();
+                } catch (IllegalStateException e) {
+                    exception[0] = e;
+                }
+            });
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+        assertNull(exception[0]);
+    }
+
+    @RepeatedTest(10)
+    void createMultiThreadExplodingNull() throws InterruptedException {
+        Lazy<String> lazy = LazyFactory.createMultiThreadLazy(new ExplodingNullSupplier());
+        final IllegalStateException[] exception = {null};
+        int threadAmount = 20;
+        Thread[] threads = new Thread[threadAmount];
+        String[] result = new String[threadAmount];
+        for (int i = 0; i < threadAmount; i++) {
+            final int ii = i;
+            threads[i] = new Thread(() -> {
+                try {
+                    result[ii] = lazy.get();
+                } catch (IllegalStateException e) {
+                    exception[0] = e;
+                }
+            });
+            threads[i].start();
+        }
+        for (int i = 0; i < threadAmount; ++i) {
+            threads[i].join();
+            assertEquals(result[0], result[i]);
+        }
+        assertNull(exception[0]);
+    }
+
+
+    private class ExplodingSupplier implements Supplier<String> {
+        private boolean wasCalled = false;
+        @Override
+        public String get() {
+            synchronized (this) {
+                if (!wasCalled) {
+                    wasCalled = true;
+                    return "Ok, first call!";
+                }
+                throw new IllegalStateException("Supplier has exploded");
+            }
+        }
+    }
+
+    private class ExplodingNullSupplier implements Supplier<String> {
+        private boolean wasCalled = false;
+        @Override
+        public String get() {
+            synchronized (this) {
+                if (!wasCalled) {
+                    wasCalled = true;
+                    return null;
+                }
+                throw new IllegalStateException("Supplier has exploded");
+            }
+        }
+    }
+
+    private class AddSupplier implements Supplier<Integer> {
+        private int last = 0;
+        @Override
+        public Integer get() {
+            synchronized (this) {
+                last++;
+                return last;
+            }
+        }
+    }
 }
