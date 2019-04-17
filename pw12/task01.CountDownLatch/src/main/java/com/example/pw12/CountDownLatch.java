44999ddc9f1a1d70af90e88c46d452c2d2dce31b
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-04-17 15:37:00 +0300

    init commit

---
+++ pw12/task01.CountDownLatch/src/main/java/com/example/pw12/CountDownLatch.java
@@ -0,0 +1,4 @@
+package com.example.pw12;
+
+public class CountDownLatch {
+}
---
+++ pw12/task01.CountDownLatch/src/test/java/com/example/pw12/CountDownLatchTest.java
@@ -0,0 +1,7 @@
+package com.example.pw12;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CountDownLatchTest {
+
+}
