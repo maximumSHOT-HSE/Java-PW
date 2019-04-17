bed157256fd9bdbbf2c53ef4b99f3eb502902905
Ivan Shershnev <ivankrut856@yandex.ru>
2019-04-17 16:58:43 +0300

    tst

--- pw12/task01.CountDownLatch/src/test/java/com/example/pw12/CountDownLatchTest.java
+++ pw12/task01.CountDownLatch/src/test/java/com/example/pw12/CountDownLatchTest.java
@@ -3,6 +3,9 @@ package com.example.pw12;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountDownLatchTest {
@@ -22,16 +25,38 @@ class CountDownLatchTest {
     public void testSimpleAwait() {
         Thread actor = new Thread(this::actorForSimpleAwait);
         actor.start();
-        latch.await();
+        assertDoesNotThrow(() -> latch.await());
     }
+
     private void actorForSimpleAwait() {
         while (latch.getCount() != 0) {
-            latch.countDown();
-            try {
-                Thread.sleep(200);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            assertDoesNotThrow(() -> latch.countDown());
+            assertDoesNotThrow(() -> Thread.sleep(300));
         }
+    }
+
+    @Test
+    public void testCountDownLock() {
+        latch = new CountDownLatch(1);
+        List<Thread> actors = new ArrayList<Thread>();
+        for (int i = 0; i < 10; i++) {
+            actors.add(new Thread(this::actorForCountDownLock));
+            actors.get(i).start();
+        }
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            assertDoesNotThrow(() -> actors.get(finalI).join());
+        }
+    }
+
+    private void actorForCountDownLock() {
+        System.out.println("Before CountDown at " + Thread.currentThread().getName());
+        assertDoesNotThrow(() -> latch.countDown());
+        System.out.println("CountDown at " + Thread.currentThread().getName());
+        assertDoesNotThrow(() -> Thread.sleep(200));
+        System.out.println("Slept at " + Thread.currentThread().getName());
+        assertDoesNotThrow(() -> latch.countUp());
+        System.out.println("CountUp at " + Thread.currentThread().getName());
+        System.out.println("Latch count is " + latch.getCount() + " at " + Thread.currentThread().getName());
     }
 }
