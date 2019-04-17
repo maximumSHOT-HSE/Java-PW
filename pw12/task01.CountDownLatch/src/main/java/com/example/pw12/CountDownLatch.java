ac8bc209ac7f93d9a5198372e17f20b35638cb52
Ivan Shershnev <ivankrut856@yandex.ru>
2019-04-17 16:43:25 +0300

    local

--- pw12/task01.CountDownLatch/src/main/java/com/example/pw12/CountDownLatch.java
+++ pw12/task01.CountDownLatch/src/main/java/com/example/pw12/CountDownLatch.java
@@ -1,5 +1,4 @@
 package com.example.pw12;
 
 public class CountDownLatch {
-
 }
--- pw12/task01.CountDownLatch/src/test/java/com/example/pw12/CountDownLatchTest.java
+++ pw12/task01.CountDownLatch/src/test/java/com/example/pw12/CountDownLatchTest.java
@@ -1,7 +1,38 @@
 package com.example.pw12;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CountDownLatchTest {
+    private CountDownLatch latch;
+    private final int baseCntSize = 10;
 
+    @BeforeEach
+    public void init() {
+        latch = new CountDownLatch(baseCntSize);
+    }
+
+    @Test
+    public void testInit() {
+    }
+
+    @Test
+    public void testSimpleAwait() {
+        Thread actor = new Thread(this::actorForSimpleAwait);
+        actor.start();
+        latch.await();
+    }
+
+    private void actorForSimpleAwait() {
+        while (latch.getCount() != 0) {
+            latch.countDown();
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
