a9a7e803a2e74958d1c345763a206666d9de5682
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-04-17 16:41:24 +0300

    add signatures of methods

--- pw12/task01.CountDownLatch/src/main/java/com/example/pw12/CountDownLatch.java
+++ pw12/task01.CountDownLatch/src/main/java/com/example/pw12/CountDownLatch.java
@@ -1,5 +1,68 @@
 package com.example.pw12;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class CountDownLatch {
 
+    private Lock lock = new ReentrantLock();
+    private Condition isZero = lock.newCondition();
+    private Condition isPositive = lock.newCondition();
+    private int count;
+
+    public CountDownLatch(int count) {
+        if (count < 0) {
+            throw new IllegalArgumentException("count should be a non-negative value, but found count = " + count);
+        }
+        this.count = count;
+    }
+
+    public void await() throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        lock.lock();
+        try {
+            while (count != 0) {
+                isZero.await();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void countDown() throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        lock.lock();
+        try {
+            while (count == 0) {
+                isPositive.await();
+            }
+            count--;
+            if (count == 0) {
+                isZero.signalAll();
+            }
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void countUp() throws InterruptedException {
+        if (Thread.interrupted()) {
+            throw new InterruptedException();
+        }
+        lock.lock();
+        try {
+
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public int getCount() {
+        return count;
+    }
 }
