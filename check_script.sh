02cee93dd6d02cb3d9e013b7b43ffe612fbf3177
Maxim Surkov (Ubuntu 18.04) <surkovmax007@mail.ru>
2019-05-29 14:11:11 +0300

    fix travis

---
+++ check_script.sh
@@ -0,0 +1,15 @@
+  
+for PROJECT_DIR in */
+do 
+  cd $PROJECT_DIR
+  for TASK_DIR in */
+  do
+      cd $TASK_DIR
+      if [[ -f "gradlew" ]]
+      then
+          ./gradlew check
+      fi
+      cd ..
+  done
+  cd ..
+done 
